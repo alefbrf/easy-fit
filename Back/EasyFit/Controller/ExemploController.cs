ab674d9bc891ff51a031973b28b86b2af65a8c06
Alef Frade <alef.frade.01tec@gmail.com>
2023-10-16 22:05:50 -0300

    EF-1

---
+++ Back/EasyFit/Controller/ExemploController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyFit.Controller
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExemploController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetTest()
+        {
+            return Ok("Teste");
+        }
+    }
+}
--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
 function App() {
+  const [exemplo, setExemplo] = useState("");
+
+  useEffect(() => {
+    axios.get('https://localhost:7183/Exemplo')
+      .then(function (response) {
+        console.log(response);
+        setExemplo(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
 
   return (
     <>
       <h1>Hello World!</h1>
+      <h2>{exemplo}</h2>
     </>
   )
 }
